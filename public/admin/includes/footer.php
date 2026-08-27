12d256955b3782e7caffad79c46c59dd1b8639ac
chethiya216 <chethiyalakshan216@gmail.com>
2026-08-27 12:48:36 +0530

    Made header footer sidebar and index dynamic

---
+++ public/admin/includes/footer.php
@@ -0,0 +1,16 @@
+<footer class="content-footer footer bg-footer-theme">
+  <div class="container-fluid d-flex align-items-center justify-content-between py-3">
+    <div>
+      © <?php echo date("Y"); ?>, made with ❤️ by
+      <a href="https://themeselection.com" target="_blank" class="footer-link fw-bolder">ThemeSelection</a>
+    </div>
+    <div>
+      <a href="https://themeselection.com/license/" class="footer-link me-4" target="_blank">License</a>
+      <a href="https://themeselection.com/" target="_blank" class="footer-link me-4">More Themes</a>
+      <a href="https://themeselection.com/demo/sneat-bootstrap-html-admin-template/documentation/" target="_blank"
+        class="footer-link me-4">Documentation</a>
+      <a href="https://github.com/themeselection/sneat-html-admin-template-free/issues" target="_blank"
+        class="footer-link">Support</a>
+    </div>
+  </div>
+</footer>
