07c6b1b598585b98a36107eacac30d8296fbea0a
chethiya216 <chethiyalakshan216@gmail.com>
2026-09-11 01:27:07 +0530

    Added Users tab to sidebar

--- public/admin/includes/sidebar.php
+++ public/admin/includes/sidebar.php
@@ -22,6 +22,27 @@
     <li class="menu-item">
       <a href="javascript:void(0);" class="menu-link menu-toggle">
         <i class="menu-icon tf-icons bx bx-file-find"></i>
+        <div data-i18n="Prescription">Users</div>
+      </a>
+
+      <ul class="menu-sub">
+        <li class="menu-item">
+          <a href="#" class="menu-link">
+            <div data-i18n="Manage">Manage Staff</div>
+          </a>
+        </li>
+
+        <li class="menu-item">
+          <a href="#" class="menu-link">
+            <div data-i18n="Verify">Manage Users</div>
+          </a>
+        </li>
+      </ul>
+    </li>
+
+    <li class="menu-item">
+      <a href="javascript:void(0);" class="menu-link menu-toggle">
+        <i class="menu-icon tf-icons bx bx-file-find"></i>
         <div data-i18n="Prescription">Prescription</div>
       </a>
 
